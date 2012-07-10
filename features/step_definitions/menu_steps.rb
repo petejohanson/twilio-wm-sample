004856f56b9446942f2fda2c2359da3b2088e443
Peter Johanson <peter@peterjohanson.com>
2012-07-09 21:36:53 -0400

    Add a feature outlining the intro menu.

--- features/step_definitions/menu_steps.rb
+++ features/step_definitions/menu_steps.rb
@@ -1,9 +1,31 @@
+require 'rexml/document'
+
+Given /^I am in the (.*) menu$/ do |menu|
+  get "/#{menu}"
+end
 
 When /^answering a new phone call$/ do
   post '/new'
 end
 
-Then /^I am sent to the (.*) menu$/ do |m|
-  last_response.should be_redirected_to "/#{m}"
+When /^entering the (.*) menu$/ do |menu|
+  get "/#{menu}"
 end
 
+When /^I press (\d)$/ do |digit|
+  post last_request.url, { :Digits => digit }
+end
+
+Then /^I am sent to the (.*) menu$/ do |menu|
+  last_response.should be_redirected_to "/#{menu}"
+end
+
+Then /^I am prompted for a number$/ do
+  doc = REXML::Document.new(last_response.body.to_s)
+  REXML::XPath.first(doc, "/Response/Gather")
+end
+
+Then /^I hear the message '(.*)'$/ do |msg|
+  doc = REXML::Document.new(last_response.body.to_s)
+  REXML::XPath.first(doc, "//Say/text('#{msg}')")
+end
